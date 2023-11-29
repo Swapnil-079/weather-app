e95d185f4226e34bcc0fc869e5d8fc4c4c2ee1aa
Swapnil-079 <132097440+Swapnil-079@users.noreply.github.com>
2023-11-30 01:45:12 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f81a443fade35a84d7efd81dcd058652",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
